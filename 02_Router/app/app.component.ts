b61e68c312eb79817d53c938c1db423389135a8d
Zhao wenjun <zhaowenjunzz@gmail.com>
2016-07-14 17:55:13 +0800

    extract menu to component.

--- 02_Router/app/app.component.ts
+++ 02_Router/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
 import {HELLO_DIRECTIVES} from "./hello/hello";
 import {CHILD_ROUTER_DIRECTIVES} from "./childrouter/childrouter";
+import {MENU_DIRECTIVES} from "./menu/menu";
 
 @Component({
     selector: "study-angular-app",
     templateUrl: "app/app.component.html",
     styleUrls: ["app/app.component.css"],
-    directives: [ROUTER_DIRECTIVES, HELLO_DIRECTIVES, CHILD_ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES, HELLO_DIRECTIVES, CHILD_ROUTER_DIRECTIVES, MENU_DIRECTIVES],
     precompile: [HELLO_DIRECTIVES, CHILD_ROUTER_DIRECTIVES],
 })
-export default class AppComponent {}
+export default class AppComponent { }
---
+++ 02_Router/app/menu/menu.ts
@@ -0,0 +1,11 @@
+import MenuComponent from "./component/menu.component";
+
+const MENU_DIRECTIVES: any[] = [
+    MenuComponent,
+];
+
+export {
+MENU_DIRECTIVES,
+
+MenuComponent,
+};
